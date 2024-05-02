217be4233ea626021b4e774758ff6344c33c090f
Oskar Giles <Oskar@Hackbook.local>
2024-05-02 20:31:48 +0100

    [OG] Multiplayer 1st test

--- client.py
+++ client.py
@@ -1,61 +1,35 @@
 import pygame
 from network import Network
+from player import Player
 width = 500
 height = 500
 win = pygame.display.set_mode((width, height))
 pygame.display.set_caption("Clit")
 
-clientNumber = 0
-
-class Player():
-    def __init__(self,x,y,width,height,colour):
-        self.x = x
-        self.y = y
-        self.width = width
-        self.height = height
-        self.colour = colour
-        self.rect = (x,y,width,height)
-        self.vel = 3
-    def draw(self,win):
-        pygame.draw.rect(win, self.colour, self.rect)
-     
-    def move(self):
-        keys = pygame.key.get_pressed()
-
-        if keys[pygame.K_a]:
-            self.x -= self.vel
-
-        if keys[pygame.K_d]:
-            self.x += self.vel
-
-        if keys[pygame.K_w]:
-            self.y -= self.vel
-
-        if keys[pygame.K_s]:
-            self.y += self.vel
-        self.rect = (self.x,self.y,self.width, self.height)
 
 
 
-
-def redrawWindow(win,p):
+def redrawWindow(win,p, player2):
     win.fill((255,255,255))
     p.draw(win)
+    player2.draw(win)
     pygame.display.update()
 
 def main():
     run = True
     n = Network()
-    startPos = n.getPos()   
-    p = Player(50,50,100,100,(0,255,0))
+    p = n.getP()
+
     clock = pygame.time.Clock()
     while run:
         clock.tick(60)
+        p2 = n.send(p)
+
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run = False
                 pygame.quit()
         
         p.move()
-        redrawWindow(win , p)
+        redrawWindow(win , p, p2)
 main()
--- server.py
+++ server.py
@@ -1,7 +1,8 @@
 import socket
 from _thread import *
 import sys
-
+from player import Player
+import pickle
 server = "192.168.1.119"
 port = 5555
 
@@ -15,29 +16,40 @@ except socket.error as e:
 s.listen(2)
 print("Waiting for a connection, Server Started")
 
-def threaded_client(conn):
-    conn.send(str.enconde("Connected"))
+
+players = [Player(0,0,50,50,(255,0,0)), Player(100,100,50,50,(0,0,255))]
+
+def threaded_client(conn, player):
+    conn.send(pickle.dumps(players[player]))
     reply = ""
     while True:
         try:
-            data = conn.recv(2048)
-            reply = data.decode("utf-8") # Tells the console what language to understand the code in
+            data = pickle.loads(conn.recv(2048))
+            players[player] = data
+
+            
+
 
             if not data: #If no data is being recieved
                 print("Disconnected")
                 break
             else:
-                print("Recieved: ", reply)
+                if player == 1:
+                    reply = players[0]
+                else:
+                    reply = players[1]
+                print("Recieved: ", data)
                 print("Sending: ", reply)
-            conn.sendall(str.encode(reply)) #encodes data
+            conn.sendall(pickle.dumps(reply)) #encodes data
         except:
             break
     print("Lost connection")
     conn.close()
 
-
+currentPlayer = 0
 while True:
     conn, addr = s.accept() #accepts any connections 
     print("Connected to :", addr)
 
-    start_new_thread((threaded_client, (conn,)))
+    start_new_thread(threaded_client, (conn, currentPlayer))
+    currentPlayer += 1
